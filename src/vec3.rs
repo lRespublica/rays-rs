ab9449597a19ffe139ee17c3eaf956e670a1f803
Leonid Znamenok <respublica@altlinux.org>
2026-08-03 18:33:26 +0400

    vec3: add new() constructor

--- src/vec3.rs
+++ src/vec3.rs
@@ -11,4 +11,6 @@ impl<T> Vec3<T> {
     pub fn map<V, F: Fn(T) -> V> (self, f: F) -> Vec3<V> {
         Vec3(f(self.0), f(self.1), f(self.2))
     }
+
+    pub fn new(a: T, b: T, c: T) -> Vec3<T> {Vec3(a, b, c)}
 }
