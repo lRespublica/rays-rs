6d956f39db297f6b2d46208efe328c50253b2bb0
Leonid Znamenok <respublica@altlinux.org>
2026-08-08 01:27:04 +0400

    png: use rayon

--- src/format/png.rs
+++ src/format/png.rs
@@ -5,6 +5,8 @@ use bytemuck;
 
 use miniz_oxide;
 
+use rayon::prelude::*;
+
 use crate::image::Image;
 use crate::color::RGB;
 
@@ -14,14 +16,17 @@ pub fn write<W: Write> (img: &Image<RGB>, w: &mut W) -> io::Result<()>{
         [w[0], w[1], w[2], w[3], h[0], h[1], h[2], h[3], 8, 2, 0, 0, 0]
     };
 
-    let mut idat_raw: Vec<u8> = Vec::with_capacity(img.height() as usize * (img.width()*3 + 1) as usize);
+    let stride = img.width() as usize * 3 + 1;
+    let mut idat_raw = vec![0u8; img.height() as usize * stride];
 
-    for c in img.raw().chunks(img.width() as usize) {
-        idat_raw.push(0);
-        let _ = idat_raw.write_all(bytemuck::cast_slice(c));
-    }
+    idat_raw.par_chunks_mut(stride)
+            .zip_eq(img.raw().par_chunks(img.width() as usize))
+            .for_each(|(dst, row)| {
+                dst[0] = 0;
+                dst[1..].copy_from_slice(bytemuck::cast_slice(row));
+            });
 
-    let idat = miniz_oxide::deflate::compress_to_vec_zlib(&idat_raw, 8);
+    let idat = miniz_oxide::deflate::compress_to_vec_zlib(&idat_raw, 0);
 
     w.write_all(b"\x89PNG\r\n\x1a\n")?;
     write_chunk(w, b"IHDR", &ihdr)?;
