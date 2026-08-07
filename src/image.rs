98963d5a34aee6297f1cfeff5696815266303ae3
Leonid Znamenok <respublica@altlinux.org>
2026-08-08 01:26:51 +0400

    image: use rayon

--- src/image.rs
+++ src/image.rs
@@ -1,12 +1,14 @@
 use strength_reduce::StrengthReducedU32;
 
+use rayon::prelude::*;
+
 use crate::color::{Color, RGB};
 
 mod private {
     pub trait Sealed {}
 }
 
-pub trait Pixel: private::Sealed + Copy {}
+pub trait Pixel: private::Sealed + Copy + Send + Sync {}
 
 impl private::Sealed for Color {}
 impl private::Sealed for RGB   {}
@@ -25,10 +27,14 @@ impl<T: Pixel> Image<T> {
     pub fn height(&self) -> u32 {self.height}
     pub fn raw(&self)    -> &[T]  {&self.pixels}
 
-    pub fn from_fn<F: Fn(u32, u32) -> T> (width: u32, height: u32, f: F) -> Image<T> {
+    pub fn from_fn<F> (width: u32, height: u32, f: F) -> Image<T>
+    where
+        F: Fn(u32, u32) -> T + Send + Sync,
+    {
         let w = StrengthReducedU32::new(width);
 
         let pixels = (0..width*height)
+            .into_par_iter()
             .map(|i| {
                 let (y, x) = StrengthReducedU32::div_rem(i, w);
                 f (x, y)
